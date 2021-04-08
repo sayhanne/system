33f13b88df432828553df390ca286eb69666321d
sayhanne <sayhannee@gmail.com>
2021-04-08 18:11:34 +0000

    Resolved issue #2

--- ldap-ad.py
+++ ldap-ad.py
@@ -1,23 +1,43 @@
 import ldap
+import ldap.modlist as modlist
 import getpass
-
 def ldap_initialize(remote, port, user, password):
+    global conn
+    server = 'ldap://' + remote  # not secure connection
+    conn = ldap.initialize(uri=server)
     try:
-        server = 'ldap://' + remote #+ ':' + port
-        l = ldap.initialize(server)
-        l.protocol_version = ldap.VERSION3
-        l.set_option(ldap.OPT_REFERRALS, 0)
-        l.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
-        l.set_option(ldap.OPT_NETWORK_TIMEOUT, 20.0)
-        bind = l.simple_bind_s(user, password)
+        conn.protocol_version = ldap.VERSION3
+        conn.set_option(ldap.OPT_REFERRALS, 0)
+        conn.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+        bind = conn.simple_bind_s(user, password)
         print("Successfully bound to %s.\n" %server)
     except ldap.LDAPError as e:
         print(str(e))
 
+def create_user(name, surname, phone_number):
+    global conn
+    base_dn = 'CN=Users,DC=hnn,DC=local'
+    user_dn = 'CN=' + name + ' ' + surname + ',' + base_dn 
+    full_name = name + ' ' + surname
+    try:
+        user_attrs = {}
+        user_attrs['objectclass'] = [b'top', b'person', b'organizationalPerson', b'user']
+        user_attrs['cn'] = full_name.encode("utf-8")
+        user_attrs['givenName'] = name.encode("utf-8")
+        user_attrs['sn'] = surname.encode("utf-8")
+        user_attrs['displayName'] = full_name.encode("utf-8")
+        user_attrs['telephoneNumber'] = phone_number.encode("utf-8")
+        user_ldif = modlist.addModlist(user_attrs)
+        conn.add_s(user_dn, user_ldif)
+        print('New user added succesfully!')
+    except ldap.LDAPError as e:
+        print(str(e))
 
 def main():
-    user = input('Enter user@domain:')
-    password = getpass.getpass('Enter password:')
-    ldap_initialize(remote='192.168.1.48', port='636', user=user, password=password)
+    ldap_initialize(remote='192.168.1.60', port='636', user='administrator@hnn.local', password='ramY8.')
+    name = input('Enter name:')
+    surname = input('Enter surname:')
+    phone_number = input('Enter phone number:')
+    create_user(name=name, surname=surname, phone_number=phone_number)
 
 main()
