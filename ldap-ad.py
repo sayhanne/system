d1756d148a06b33667df4d81838aa69ee8db81a2
sayhanne <sayhannee@gmail.com>
2021-04-09 12:39:50 +0000

    Issue #4 attributes pulled (Unfinished work)

--- ldap-ad.py
+++ ldap-ad.py
@@ -56,14 +56,35 @@ def get_user(name):
     except ldap.LDAPError as e:
         print(str(e))
 
+def get_user_attributes(name, attribute_list):
+    global conn
+    try:
+        base = 'DC=hnn,DC=local'
+        criteria = "(givenName=" + name + ")"
+        attributes = attribute_list
+        print('Searching for name:%s' % (name))
+        print('-------------')
+        result = conn.search_s(base, ldap.SCOPE_SUBTREE, criteria, attributes)
+        if len(result) != 0:
+            for dn, attr in result:
+                print('User DN:%s' % dn)
+                for key, value in attr.items():
+                    print('Attribute:' + key + '->' + value[0].decode('utf-8'))
+                print('-------------')
+        else:
+            print('User not found!')
+    except ldap.LDAPError as e:
+        print(str(e))
 
 def main():
+    global conn
     ldap_initialize(remote='192.168.1.60', port='636', user='administrator@hnn.local', password='ramY8.')
     # name = input('Enter name:')
     # surname = input('Enter surname:')
     # phone_number = input('Enter phone number:')
     # create_user(name=name, surname=surname, phone_number=phone_number)
-    get_user(name='Jane')
-
+    # get_user(name='Jane')
+    get_user_attributes(name='Jane', attribute_list=['cn', 'telephoneNumber'])
+    conn.unbind()
 
 main()
