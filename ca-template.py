4dff8781f6037dc074a61d489283c9bf781fd668
sayhanne <sayhannee@gmail.com>
2021-04-20 15:19:01 +0000

    Resolved issue #6

--- ca-template.py
+++ ca-template.py
@@ -1,5 +1,9 @@
 import ldap
 import ldap.modlist as modlist
+import base64
+
+conn = None
+base = 'CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,DC=hnn,DC=local'
 
 def ldap_initialize(remote, port, user, password):
     global conn
@@ -20,16 +24,12 @@ def ldap_initialize(remote, port, user, password):
 
 
 def get_template(name):
-    global conn
-    base = 'CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,DC=hnn,DC=local'
     criteria = '(cn='+ name +')'
     result = conn.search_s(base, ldap.SCOPE_SUBTREE, criteria)
     print(result)
 
 def create_template(name):
-    global conn
-    base_dn = 'CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,DC=hnn,DC=local'
-    template_dn = 'CN=' + name + ',' + base_dn
+    template_dn = 'CN=' + name + ',' + base
     default_csps=[b'2,Microsoft Base Cryptographic Provider v1.0', b'1,Microsoft Enhanced Cryptographic Provider v1.0']
     try:
         template_attr = {}
@@ -43,12 +43,33 @@ def create_template(name):
     except ldap.LDAPError as e:
         print(str(e))
 
+def ssl_ad_sync():
+    criteria = '(cn=Machine)'
+    attributes = ['pKIKeyUsage', 'pKIExtendedKeyUsage', 'pKIMaxIssuingDepth']
+    result = conn.search_s(base, ldap.SCOPE_SUBTREE, criteria, attributes)
+    attribute_dict = result[0][1]
+    key_hex = attribute_dict['pKIKeyUsage'][0]
+    # print(key_hex)
+    flags = list(bytearray(key_hex))
+    subset_sum([1,2,4,8,16,32,64,128,32768],flags[0])
+
+def subset_sum(numbers, target, partial=[]):
+    s = sum(partial)
+
+    # check if the partial sum is equals to target
+    if s == target: 
+        print("sum(%s)=%s" % (partial, target))
+    if s >= target:
+        return  # if we reach the number then end
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        remaining = numbers[i+1:]
+        subset_sum(remaining, target, partial + [n])
 
 def main():
-    global conn
     ldap_initialize(remote='192.168.1.60', port=636, user='administrator@hnn.local', password='ramY8.')
-    create_template('TemplateDemo')
-    get_template('TemplateDemo')
+    ssl_ad_sync()
     conn.unbind()
 
 
